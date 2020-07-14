057c13e7faf5e13efe3c344f36d076887acd516c
Rafael Akiyoshi <rafah94@hotmail.com>
2020-07-14 17:35:15 -0400

    updating index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import { Switch, Card } from "antd";
 
-class Index extends React.Component {
-  render() {
-    return (
-      <div>Hayai</div>
-    );
+import { useThemeSwitcher } from "react-css-theme-switcher";
+
+export default function Home() {
+  const [isDarkMode, setIsDarkMode] = React.useState();
+  const { switcher, currentTheme, status, themes } = useThemeSwitcher();
+
+  const toggleTheme = (isChecked) => {
+    setIsDarkMode(isChecked);
+    switcher({ theme: isChecked ? themes.dark : themes.light });
+  };
+
+  if (status === "loading") {
+    return null;
   }
-}
 
-export default Index;
+  return (
+    <>
+      <div className="welcome">
+        <Card style={{ width: 300, textAlign: "center", marginTop: "10%" }}>
+          <h1>Hayai!</h1>
+          <h4>Boilerplate</h4>
+          <Switch checked={isDarkMode} onChange={toggleTheme} />
+        </Card>
+      </div>
+      <style jsx>{`
+        .welcome {
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        h1 {
+          font-size: 50px;
+          margin-bottom: 0;
+        }
+        h4 {
+          font-size: 20px;
+          margin-top: 0;
+        }
+      `}</style>
+    </>
+  );
+}
