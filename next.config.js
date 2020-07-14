5004a75c6073928879233fd9cffaf2b79166fd67
Rafael Akiyoshi <rafah94@hotmail.com>
2020-07-13 21:11:57 -0400

    less integration with boilerplate

--- next.config.js
+++ next.config.js
@@ -33,12 +33,40 @@ const compose = (plugins) => ({
 });
 
 const withBundleAnalyzer = require("@next/bundle-analyzer");
-
+const withLess = require("@zeit/next-less");
 module.exports = compose([
   [
     withBundleAnalyzer,
     {
       enabled: process.env.ANALYZE === "true",
     },
+    withLess({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === "function") {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === "function" ? [] : origExternals),
+          ];
+    
+          config.module.rules.unshift({
+            test: antStyles,
+            use: "null-loader",
+          });
+        }
+        return config;
+      },
+    }),
   ],
 ]);
