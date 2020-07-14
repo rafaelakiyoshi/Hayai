623eecd53ee3621536c2c7e4a9664cd2af9b3295
Rafael Akiyoshi <rafah94@hotmail.com>
2020-07-13 21:14:05 -0400

    adding themeswitcher to boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
-import 'antd/dist/antd.compact.css';
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Hayai Boilerplate</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+const themes = {
+  dark: `/dark-theme.module.css`,
+  light: `/light-theme.module.css`,
+};
+
+export default class MyApp extends React.Component {
+  state = {
+    rendered: true
+  }
+  componentDidMount() {
+    this.setState({ rendered: true });
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    const { rendered } = this.state;
+    return (
+      <>
+        <Head>
+          <title>Hayai Boilerplate</title>
+        </Head>
+        <ThemeSwitcherProvider themeMap={themes} defaultTheme="light" insertionPoint="styles-insertion-point">
+          <Component {...pageProps} />
+        </ThemeSwitcherProvider>
+      </>
+    );
+  }
 }
